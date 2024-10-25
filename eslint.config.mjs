7ec6220943898b6815b22472639df9fef3927ea0
Joseph Lozano <joseph.lozano@gusto.com>
2024-10-25 13:00:12 -0400

    feat: add common Combobox input

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -58,7 +58,7 @@ export default [
           paths: [
             {
               name: 'react-aria-components',
-              importNames: ['TextField'],
+              importNames: ['TextField', 'ComboBox'],
               message: 'Please use the TextField component from @/components/Common instead.',
             },
           ],
