d203614105fa60cdfcc41308d1b6d34cb81ad745
Aaron Rosen <aaron.rosen@gusto.com>
2024-11-15 08:53:36 -0700

    setup auto-generated ts sdk to try out

---
+++ scripts/gusto_client.ts
@@ -0,0 +1,15 @@
+import { Gusto } from 'gusto'
+
+const gusto = new Gusto({
+  companyAccessAuth: process.env['GUSTO_COMPANY_ACCESS_AUTH'] ?? '',
+})
+
+async function run() {
+  const request = {}
+  const result = await gusto.introspection.getInfo(request)
+
+  // Handle the result
+  console.log(result)
+}
+
+run()
