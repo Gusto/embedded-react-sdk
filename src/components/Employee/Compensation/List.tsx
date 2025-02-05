f023711b2be2808fa63b0a7efa4c0209b9bf651e
Jeff Johnson <jeff.johnson@gusto.com>
2025-02-05 09:00:30 -0800

    WIP: Compensation to DataView

--- src/components/Employee/Compensation/List.tsx
+++ src/components/Employee/Compensation/List.tsx
@@ -1,69 +1,73 @@
-import { VisuallyHidden } from 'react-aria'
-import { Cell, Column, Row, Table, TableBody, TableHeader } from 'react-aria-components'
 import { useTranslation } from 'react-i18next'
 import PencilSvg from '@/assets/icons/pencil.svg?react'
 import TrashCanSvg from '@/assets/icons/trashcan.svg?react'
-import { Hamburger, HamburgerItem } from '@/components/Common'
+import { DataView, Hamburger, HamburgerItem, useDataView } from '@/components/Common'
 import { useCompensation } from './Compensation'
 
 export const List = () => {
   const { employeeJobs, mode, isPending, handleEdit, handleDelete } = useCompensation()
   const { t } = useTranslation('Employee.Compensation')
+  const { ...dataViewProps } = useDataView({
+    columns: [
+      {
+        key: 'title',
+        title: t('allCompensations.jobColumn'),
+      },
+      {
+        key: 'flsa_status',
+        title: t('allCompensations.typeColumn'),
+        render: job => {
+          let returnNode = ''
+          const flsaStatus = job.compensations?.find(
+            comp => comp.uuid === job.current_compensation_uuid,
+          )?.flsa_status
+
+          if (flsaStatus !== undefined) {
+            returnNode = t(`flsaStatusLabels.${flsaStatus}`)
+          }
+
+          return returnNode
+        },
+      },
+      {
+        key: 'rate',
+        title: t('allCompensations.amountColumn'),
+      },
+      {
+        key: 'payment_unit',
+        title: t('allCompensations.perColumn'),
+      },
+    ],
+    data: employeeJobs,
+    itemMenu: job => {
+      return (
+        <Hamburger title={t('hamburgerTitle')} isPending={isPending}>
+          <HamburgerItem
+            icon={<PencilSvg aria-hidden />}
+            onAction={() => {
+              handleEdit(job.uuid)
+            }}
+          >
+            {t('allCompensations.editCta')}
+          </HamburgerItem>
+          {!job.primary && (
+            <HamburgerItem
+              icon={<TrashCanSvg aria-hidden />}
+              onAction={() => {
+                handleDelete(job.uuid)
+              }}
+            >
+              {t('allCompensations.deleteCta')}
+            </HamburgerItem>
+          )}
+        </Hamburger>
+      )
+    },
+  })
 
   if (mode !== 'LIST') {
     return
   }
 
-  return (
-    <>
-      <Table aria-label={t('allCompensations.tableLabel')}>
-        <TableHeader>
-          <Column isRowHeader>{t('allCompensations.jobColumn')}</Column>
-          <Column>{t('allCompensations.typeColumn')}</Column>
-          <Column>{t('allCompensations.amountColumn')}</Column>
-          <Column>{t('allCompensations.perColumn')}</Column>
-          <Column>
-            <VisuallyHidden>{t('allCompensations.actionColumn')}</VisuallyHidden>
-          </Column>
-        </TableHeader>
-        <TableBody>
-          {employeeJobs.map(job => {
-            const flsaStatus = job.compensations?.find(
-              comp => comp.uuid === job.current_compensation_uuid,
-            )?.flsa_status
-            return (
-              <Row key={job.uuid}>
-                <Cell>{job.title}</Cell>
-                <Cell>{flsaStatus !== undefined && t(`flsaStatusLabels.${flsaStatus}`)}</Cell>
-                <Cell>{job.rate}</Cell>
-                <Cell>{job.payment_unit}</Cell>
-                <Cell>
-                  <Hamburger title={t('hamburgerTitle')} isPending={isPending}>
-                    <HamburgerItem
-                      icon={<PencilSvg aria-hidden />}
-                      onAction={() => {
-                        handleEdit(job.uuid)
-                      }}
-                    >
-                      {t('allCompensations.editCta')}
-                    </HamburgerItem>
-                    {!job.primary && (
-                      <HamburgerItem
-                        icon={<TrashCanSvg aria-hidden />}
-                        onAction={() => {
-                          handleDelete(job.uuid)
-                        }}
-                      >
-                        {t('allCompensations.deleteCta')}
-                      </HamburgerItem>
-                    )}
-                  </Hamburger>
-                </Cell>
-              </Row>
-            )
-          })}
-        </TableBody>
-      </Table>
-    </>
-  )
+  return <DataView label={t('allCompensations.tableLabel')} {...dataViewProps} />
 }
