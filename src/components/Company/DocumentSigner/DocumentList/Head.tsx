825ebce56e018563419851099c9223eb8c69b712
Steve Jensen <steve.jensen@gusto.com>
2025-02-20 16:23:22 -0700

    feat: add company signature form

--- src/components/Company/DocumentSigner/DocumentList/Head.tsx
+++ src/components/Company/DocumentSigner/DocumentList/Head.tsx
@@ -1,7 +1,7 @@
 import { useTranslation } from 'react-i18next'
 
 export function Head() {
-  const { t } = useTranslation('Company.DocumentSigner')
+  const { t } = useTranslation('Company.DocumentList')
 
   return <h2>{t('documentListTitle')}</h2>
 }
