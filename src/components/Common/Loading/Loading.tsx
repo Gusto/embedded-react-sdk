d17b9bd136a655d442fa9e235e06a4558f4ec688
Aaron Lee <aaron.lee@gusto.com>
2025-03-14 15:42:36 -0400

    style: fix linter error flagging redundant null checking

--- src/components/Common/Loading/Loading.tsx
+++ src/components/Common/Loading/Loading.tsx
@@ -1,9 +1,16 @@
+import { useTranslation } from 'react-i18next'
 import cn from 'classnames'
 import styles from './Loading.module.scss'
 
 export const Loading = () => {
+  const { t } = useTranslation('common')
   return (
-    <section className={styles.skeletonContainer} aria-live="polite" aria-busy>
+    <section
+      className={styles.skeletonContainer}
+      aria-label={t('status.loading')}
+      aria-live="polite"
+      aria-busy
+    >
       <div className={cn(styles.skeleton, styles.skeletonBox)}></div>
     </section>
   )
