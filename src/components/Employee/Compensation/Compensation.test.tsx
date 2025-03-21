f96e4001ac32a84c4937f4c51974b3ae769ee39c
Dmitriy Abragamov <dmitriy.abragamov@gusto.com>
2025-03-21 13:28:47 -0400

    fix: cleanup

--- src/components/Employee/Compensation/Compensation.test.tsx
+++ src/components/Employee/Compensation/Compensation.test.tsx
@@ -1,5 +1,5 @@
-import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
-import { cleanup, render, screen, waitFor, within } from '@testing-library/react'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+import { render, screen, waitFor, within } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { HttpResponse } from 'msw'
 import { Compensation } from './Compensation'
