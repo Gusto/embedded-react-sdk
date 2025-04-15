d165681075cc7b211ac0d639f2d1191a3242649c
Jeff Johnson <jeff.johnson@gusto.com>
2025-04-15 15:11:10 -0700

    fix: pr feedback

--- src/components/Company/Industry/Edit.tsx
+++ src/components/Company/Industry/Edit.tsx
@@ -1,6 +1,4 @@
-import { useFormContext } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
-import { useMemo } from 'react'
 import { useIndustryItems } from './Context'
 import { ComboBoxField } from '@/components/Common'
 
@@ -12,17 +10,10 @@ export const Edit = () => {
   const { t } = useTranslation('Company.Industry')
   const { items } = useIndustryItems()
 
-  const options = useMemo(() => {
-    return items.map(item => ({
-      label: item.name,
-      value: item.id,
-    }))
-  }, [items])
-
   return (
     <ComboBoxField
       isRequired
-      options={options}
+      options={items}
       label={t('label')}
       name="naics_code"
       placeholder={t('placeholder')}
