b7d4b6b95f60aef6402dfddee571946fae4b7b70
Jeff Johnson <jeff.johnson@gusto.com>
2025-04-24 16:48:46 -0700

    fix: update style file name to plural

--- src/components/Common/UI/Breadcrumb/Breadcrumbs.tsx
+++ src/components/Common/UI/Breadcrumb/Breadcrumbs.tsx
@@ -1,6 +1,6 @@
 import { Breadcrumbs as AriaBreadcrumbs } from 'react-aria-components'
 import type { BreadcrumbsProps } from './BreadcrumbTypes'
-import styles from './Breadcrumb.module.scss'
+import styles from './Breadcrumbs.module.scss'
 
 export function Breadcrumbs({ children, className }: BreadcrumbsProps) {
   return (
