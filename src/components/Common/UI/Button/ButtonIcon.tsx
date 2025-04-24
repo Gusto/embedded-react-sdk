fda25170cd2bda68d270ad9e42b4efd3bb181668
Jeff Johnson <jeff.johnson@gusto.com>
2025-04-24 13:19:32 -0700

    fix: buttonicon should use button internally

--- src/components/Common/UI/Button/ButtonIcon.tsx
+++ src/components/Common/UI/Button/ButtonIcon.tsx
@@ -1,40 +1,6 @@
-import { Button as AriaButton } from 'react-aria-components'
 import { type ButtonIconProps } from './ButtonTypes'
-import styles from './Button.module.scss'
+import { Button } from './Button'
 
-export function ButtonIcon({
-  isError = false,
-  isLoading = false,
-  isDisabled = false,
-  ref,
-  className,
-  children,
-  onBlur,
-  onFocus,
-  onClick,
-  ...props
-}: ButtonIconProps) {
-  const handlePress = onClick
-    ? () => {
-        onClick({} as React.MouseEvent<HTMLButtonElement>)
-      }
-    : undefined
-
-  return (
-    <span className={styles.root}>
-      <AriaButton
-        {...props}
-        ref={ref}
-        onBlur={onBlur}
-        onFocus={onFocus}
-        isDisabled={isDisabled || isLoading}
-        data-variant="icon"
-        data-loading={isLoading || undefined}
-        data-error={isError || undefined}
-        onPress={handlePress}
-      >
-        {children}
-      </AriaButton>
-    </span>
-  )
+export function ButtonIcon(props: ButtonIconProps) {
+  return <Button {...props} variant="icon" />
 }
