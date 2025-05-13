506741ef422fd71c64fa9c4a156f2a827efb1347
Sean Scally <scally@users.noreply.github.com>
2025-05-13 12:45:55 -0700

    build: use webpack instead of vite for builds

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+import { join } from 'node:path'
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin'
+
+export default {
+  entry: join(import.meta.dirname, 'src', 'index.ts'),
+  experiments: {
+    outputModule: true,
+  },
+  output: {
+    path: join(import.meta.dirname, 'dist'),
+    library: {
+      type: 'module',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.png/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-inline-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    plugins: [new TsconfigPathsPlugin()],
+  },
+  stats: {
+    errorDetails: true,
+  },
+  externals: ['react', 'react/jsx-runtime', 'react-dom'],
+}
