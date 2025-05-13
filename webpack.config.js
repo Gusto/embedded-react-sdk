80bd0e563fbcd9204c4154c5311cf1ae95646926
Sean Scally <scally@users.noreply.github.com>
2025-05-13 14:30:39 -0700

    build: write css file

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 import { join } from 'node:path'
 import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 
 export default {
   entry: join(import.meta.dirname, 'src', 'index.ts'),
@@ -12,6 +13,7 @@ export default {
       type: 'module',
     },
   },
+  plugins: [new MiniCssExtractPlugin()],
   module: {
     rules: [
       {
@@ -26,8 +28,16 @@ export default {
       {
         test: /\.s[ac]ss$/i,
         use: [
-          'style-loader',
-          'css-loader',
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              esModule: true,
+              modules: {
+                namedExport: false,
+              },
+            },
+          },
           {
             loader: 'sass-loader',
             options: {
@@ -41,6 +51,9 @@ export default {
         loader: 'svg-inline-loader',
       },
     ],
+  },
+  optimization: {
+    splitChunks: false,
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
