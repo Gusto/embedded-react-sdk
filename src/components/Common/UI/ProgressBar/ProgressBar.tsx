ae65ea528aa1ecd9e4ab61cc6aa43d9b6ea1a76c
Dmitriy Abragamov <dmitriy.abragamov@gusto.com>
2025-05-15 08:12:40 -0400

    fix: format fix

--- src/components/Common/UI/ProgressBar/ProgressBar.tsx
+++ src/components/Common/UI/ProgressBar/ProgressBar.tsx
@@ -3,7 +3,7 @@ import type { ProgressBarProps } from './ProgressBarTypes'
 import styles from './ProgressBar.module.scss'
 
 export function ProgressBar({ className, totalSteps, currentStep, label }: ProgressBarProps) {
-  const clampedStep = Math.max(1, Math.min(currentStep, totalSteps));
+  const clampedStep = Math.max(1, Math.min(currentStep, totalSteps))
   const progressBarStyle = {
     '--g-progress-bar-width': `${(clampedStep * 100) / totalSteps}%`,
   } as React.CSSProperties
