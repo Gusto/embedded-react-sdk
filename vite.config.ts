e5d65e45ca00b975197de389f58e0ae952d47416
Dmitriy Abragamov <dmitriy.abragamov@gusto.com>
2025-05-19 13:19:33 -0400

    build: optimize development mode for faster build

--- vite.config.ts
+++ vite.config.ts
@@ -9,82 +9,88 @@ import circularDependencyDetector from 'vite-plugin-circular-dependency'
 import checker from 'vite-plugin-checker'
 import { externalizeDeps } from 'vite-plugin-externalize-deps'
 
-export default defineConfig({
-  plugins: [
-    react(),
-    externalizeDeps(), // Externalizes all dependencies
-    dts({
-      include: ['src'],
-      outDir: './dist',
-      tsconfigPath: './tsconfig.json',
-      insertTypesEntry: true,
-      rollupTypes: false,
-      copyDtsFiles: false, // 🚨 Important: disables copying external .d.ts files
-      exclude: [
-        '**/node_modules/**',
-        '**/.ladle/**',
-        '**/*.stories.tsx',
-        '**/*.test.tsx',
-        '**/test/**',
-      ],
-      strictOutput: true,
-    }),
-    stylelint({ fix: true }),
-    svgr({
-      svgrOptions: {
-        exportType: 'default',
-        titleProp: true,
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development'
+  return {
+    plugins: [
+      react(),
+      externalizeDeps(), // Externalizes all dependencies
+      !isDev &&
+        dts({
+          include: ['src'],
+          outDir: './dist',
+          tsconfigPath: './tsconfig.json',
+          insertTypesEntry: true,
+          rollupTypes: false,
+          copyDtsFiles: false, // 🚨 Important: disables copying external .d.ts files
+          exclude: [
+            '**/node_modules/**',
+            '**/.ladle/**',
+            '**/*.stories.tsx',
+            '**/*.test.tsx',
+            '**/test/**',
+          ],
+          strictOutput: true,
+        }),
+      !isDev && stylelint({ fix: true }),
+      svgr({
+        svgrOptions: {
+          exportType: 'default',
+          titleProp: true,
+        },
+        include: ['**/*.svg?react', '**/*.svg'],
+      }),
+      !isDev && circularDependencyDetector(),
+      !isDev &&
+        checker({
+          typescript: true,
+        }),
+    ],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
       },
-      include: ['**/*.svg?react', '**/*.svg'],
-    }),
-    circularDependencyDetector(),
-    checker({
-      typescript: true,
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
     },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'modern-compiler',
-        additionalData: `@use "@/styles/Helpers" as *; @use '@/styles/Responsive' as *;
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler',
+          additionalData: `@use "@/styles/Helpers" as *; @use '@/styles/Responsive' as *;
 `,
+        },
       },
     },
-  },
-  build: {
-    lib: {
-      fileName: 'index',
-      entry: resolve(__dirname, 'src/index.ts'),
-      formats: ['es'],
-    },
-    sourcemap: true,
-    cssCodeSplit: false, //Force vite to generate single css file
-    rollupOptions: {
-      input: resolve(__dirname, 'src/index.ts'),
-      output: {
-        preserveModules: true, //Maintains the original file structure
-        preserveModulesRoot: 'src', //Removes the root folder from the output
-        dir: 'dist',
-        entryFileNames: '[name].js', //Retains the original file name
-        manualChunks: undefined, //Disabling manual chunking
-        format: 'es',
+    build: {
+      lib: {
+        fileName: 'index',
+        entry: resolve(__dirname, 'src/index.ts'),
+        formats: ['es'],
       },
-    },
+      minify: !isDev,
+      sourcemap: !isDev,
+      cssCodeSplit: false, //Force vite to generate single css file
+      rollupOptions: {
+        input: resolve(__dirname, 'src/index.ts'),
+        output: {
+          preserveModules: true, //Maintains the original file structure
+          preserveModulesRoot: 'src', //Removes the root folder from the output
+          dir: 'dist',
+          entryFileNames: '[name].js', //Retains the original file name
+          manualChunks: undefined, //Disabling manual chunking
+          format: 'es',
+        },
+      },
 
-    target: 'es2022',
-  },
-  //Explicitely exclude ladle and react from being bundled - should only affect dev
-  optimizeDeps: {
-    exclude: ['~ladle/*', 'react', 'react-dom'],
-  },
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: ['./src/test/setup.ts'],
-  },
+      target: 'es2022',
+    },
+    //Explicitely exclude ladle and react from being bundled - should only affect dev
+    optimizeDeps: {
+      exclude: ['~ladle/*', 'react', 'react-dom'],
+    },
+    test: {
+      environment: 'jsdom',
+      globals: true,
+      setupFiles: ['./src/test/setup.ts'],
+    },
+  }
 })
