adebda835cca3a8ec80437000f932dfa861e572b
Sean Scally <scally@users.noreply.github.com>
2025-06-04 15:10:45 -0700

    feat: landing the rest of the moving parts

--- src/components/Contractor/List/index.tsx
+++ src/components/Contractor/List/index.tsx
@@ -1,13 +1,58 @@
-import { type Contractor } from '@gusto/embedded-api/models/components/contractor'
-import { DataView, useDataView } from '@/components/Common'
+import {
+  ContractorOnboardingStatus1 as OnboardingStatus,
+  type Contractor,
+} from '@gusto/embedded-api/models/components/contractor'
+import { DataView, EmptyData, Flex, useDataView } from '@/components/Common'
 import { firstLastName } from '@/helpers/formattedStrings'
-import { Badge } from '@/components/Common/UI/Badge/Badge'
 import { HamburgerMenu } from '@/components/Common/HamburgerMenu/HamburgerMenu'
 import PencilSvg from '@/assets/icons/pencil.svg?react'
+import { useComponentContext } from '@/contexts/ComponentAdapter/useComponentContext'
 
 export type ContractorListDisplay = Pick<Contractor, 'onboardingStatus' | 'firstName' | 'lastName'>
 
+export interface HeadProps {
+  count: number
+  onAdd: () => void
+}
+export function Head({ count, onAdd }: HeadProps) {
+  const { Badge, Button, Heading } = useComponentContext()
+
+  return (
+    <Flex>
+      <Heading as="h2">Contractors</Heading>
+      <Badge>{count}</Badge>
+      <Button variant="secondary" onClick={onAdd}>
+        Add another contractor
+      </Button>
+    </Flex>
+  )
+}
+
+export interface EmptyDataContractorsListProps {
+  onAdd: () => void
+}
+export function EmptyDataContractorsList({ onAdd }: EmptyDataContractorsListProps) {
+  const { Button } = useComponentContext()
+
+  return (
+    <EmptyData>
+      {`You haven't added any contractors yet`}
+      Add contractors to get them setup for payroll.
+      <Button onClick={onAdd}>Add a contractor</Button>
+    </EmptyData>
+  )
+}
+
 export function ContractorList() {
+  const { Badge } = useComponentContext()
+  const contractors = [
+    {
+      firstName: 'Sean',
+      lastName: 'Scally',
+      onboardingStatus: OnboardingStatus.AdminOnboardingReview,
+    },
+  ]
+
   const dataViewProps = useDataView<ContractorListDisplay>({
     columns: [
       {
@@ -20,13 +65,7 @@ export function ContractorList() {
         render: contractor => <Badge>{contractor.onboardingStatus}</Badge>,
       },
     ],
-    data: [
-      {
-        firstName: 'Sean',
-        lastName: 'Scally',
-        onboardingStatus: 'admin_onboarding_review',
-      },
-    ],
+    data: contractors,
     itemMenu: () => (
       <HamburgerMenu
         items={[{ label: 'Edit', icon: <PencilSvg aria-hidden />, onClick: () => {} }]}
@@ -34,10 +73,12 @@ export function ContractorList() {
         isLoading={false}
       />
     ),
+    emptyState: () => <EmptyDataContractorsList onAdd={() => {}} />,
   })
 
   return (
     <>
+      <Head count={contractors.length} onAdd={() => {}} />
       <DataView label="Contractor List" {...dataViewProps} />
     </>
   )
