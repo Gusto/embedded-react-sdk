4231e8ad105ff30df41786de4b372dcbac27f772
Sean Scally <scally@users.noreply.github.com>
2025-06-13 12:11:32 -0700

    feat: export ContractorList

---
+++ src/components/Contractor/index.ts
@@ -0,0 +1,1 @@
+export { ContractorListWithApi as ContractorList } from './List'
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 // Explicit exports available to partners
 export * as Company from './Company'
+export * as Contractor from './Contractor'
 export * as Employee from './Employee'
 export * as Flows from './Flow'
