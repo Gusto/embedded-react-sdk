9e462530f773b3d696ec9c60721d4fa8dd307bab
Dmitriy Abragamov <dmitriy.abragamov@gusto.com>
2025-06-18 11:35:49 -0400

    fix: fixing submission of empty state tax payload

--- src/components/Employee/Taxes/StateForm.tsx
+++ src/components/Employee/Taxes/StateForm.tsx
@@ -25,22 +25,21 @@ export const StateForm = () => {
         <Components.Heading as="h2">
           {t('stateTaxesTitle', { state: statesHash(state as (typeof STATES_ABBR)[number]) })}
         </Components.Heading>
-        {Array.isArray(questions) &&
-          questions.map(question => {
-            if (question.isQuestionForAdminOnly && !isAdmin) return null
-            return (
-              <QuestionInput
-                question={{
-                  ...question,
-                  key: `states.${state}.${snakeCaseToCamelCase(question.key)}`, // Its important not to convert state as it must maintain two capital letters
-                }}
-                questionType={
-                  question.key === 'fileNewHireReport' ? 'Radio' : question.inputQuestionFormat.type
-                }
-                key={question.key}
-              />
-            )
-          })}
+        {questions.map(question => {
+          if (question.isQuestionForAdminOnly && !isAdmin) return null
+          return (
+            <QuestionInput
+              question={{
+                ...question,
+                key: `states.${state}.${snakeCaseToCamelCase(question.key)}`, // Its important not to convert state as it must maintain two capital letters
+              }}
+              questionType={
+                question.key === 'fileNewHireReport' ? 'Radio' : question.inputQuestionFormat.type
+              }
+              key={question.key}
+            />
+          )
+        })}
       </Fragment>
     ) : null,
   )
