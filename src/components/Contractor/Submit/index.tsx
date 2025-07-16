050606cc9d4a1c8223a66cfe194e18a4e42b6d11
Sean Scally <scally@users.noreply.github.com>
2025-07-16 15:44:02 -0700

    refactor: use existing Alert component

--- src/components/Contractor/Submit/index.tsx
+++ src/components/Contractor/Submit/index.tsx
@@ -1,7 +1,7 @@
 import { useTranslation } from 'react-i18next'
+import styles from './index.module.scss'
 import { Flex } from '@/components/Common'
 import { useComponentContext } from '@/contexts/ComponentAdapter/useComponentContext'
-import WarningIcon from '@/assets/icons/warning.svg?react'
 import { useI18n } from '@/i18n'
 
 interface ContractorSubmitProps {
@@ -10,24 +10,20 @@ interface ContractorSubmitProps {
 
 export function ContractorSubmit({ onSubmit }: ContractorSubmitProps) {
   useI18n('Contractor.ContractorSubmit')
-  const { Button, Heading, UnorderedList } = useComponentContext()
+  const { Alert, Button, UnorderedList } = useComponentContext()
   const { t } = useTranslation('Contractor.ContractorSubmit')
   const items = Object.values(t('warningItems', { returnObjects: true }))
 
   return (
-    <Flex flexDirection="column">
-      <Flex alignItems="flex-start">
-        <WarningIcon />
-        <Flex flexDirection="column">
-          <Heading as="h4">{t('title')}</Heading>
-          <UnorderedList items={items} />
-        </Flex>
-      </Flex>
+    <>
+      <Alert label={t('title')} status="warning" className={styles.alert}>
+        <UnorderedList items={items} />
+      </Alert>
       <Flex flexDirection="column" alignItems="flex-end">
         <Button title={t('submitCTA')} onClick={onSubmit}>
           {t('submitCTA')}
         </Button>
       </Flex>
-    </Flex>
+    </>
   )
 }
