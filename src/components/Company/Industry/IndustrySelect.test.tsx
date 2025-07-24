894f719dd276347c90cbbda70be8f5118d18f8bc
Sean Scally <scally@users.noreply.github.com>
2025-07-24 14:21:37 -0700

    refactor: move virtualizer mocks to test setup
    this can create some spooky action at a distance for tests, but in this
    case we are making this tradeoff because 99% of cases don't want/need
    the real virtualizer

--- src/components/Company/Industry/IndustrySelect.test.tsx
+++ src/components/Company/Industry/IndustrySelect.test.tsx
@@ -2,21 +2,11 @@ import { beforeAll, describe, it, vi } from 'vitest'
 import { screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { mockResizeObserver } from 'jsdom-testing-mocks'
-// eslint-disable-next-line no-restricted-imports
-import type { VirtualizerProps } from 'react-aria-components'
 import { IndustrySelect } from './IndustrySelect'
 import { loadAll } from '@/models/NAICSCodes'
 import { renderWithProviders } from '@/test-utils/renderWithProviders'
 
 vi.mock('@/models/NAICSCodes')
-vi.mock(import('react-aria-components'), async originalImport => {
-  const original = await originalImport()
-  return {
-    ...original,
-    Virtualizer: <T,>({ children }: VirtualizerProps<T>) => <div>{children}</div>,
-  }
-})
-
 beforeAll(mockResizeObserver)
 
 describe('IndustrySelect', () => {
