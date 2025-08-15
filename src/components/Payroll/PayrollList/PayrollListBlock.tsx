68256b21a337f924501761f72e7bd0985d7c110f
Sean Scally <scally@users.noreply.github.com>
2025-08-15 13:31:48 -0700

    feat: convert PayrollList to block that conforms to onEvent

---
+++ src/components/Payroll/PayrollList/PayrollListBlock.tsx
@@ -0,0 +1,30 @@
+import { PayrollList } from './PayrollList'
+import type { BaseComponentInterface } from '@/components/Base'
+import { BaseComponent } from '@/components/Base'
+import { componentEvents } from '@/shared/constants'
+
+//TODO: Use Speakeasy type
+interface Company {
+  companyId: string
+}
+
+// TODO: Replace this hook with call to Speakeasy instead
+const useListCompanyPayrollsApi = ({ companyId }: Company) => {
+  return {
+    data: [{ payrollId: 'abcd' }],
+  }
+}
+
+type PayrollListBlockProps = Pick<Company, 'companyId'> & BaseComponentInterface
+
+export const PayrollListBlock = ({ companyId, onEvent }: PayrollListBlockProps) => {
+  const { data: payrolls } = useListCompanyPayrollsApi({ companyId })
+  const onRunPayroll = ({ payrollId }: { payrollId: string }) => {
+    onEvent(componentEvents.RUN_PAYROLL_SELECTED, { payrollId })
+  }
+  return (
+    <BaseComponent onEvent={onEvent}>
+      <PayrollList payrolls={payrolls} onRunPayroll={onRunPayroll} />
+    </BaseComponent>
+  )
+}
