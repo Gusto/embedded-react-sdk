336f4403460f7be08f3ea88daad588fd93156ce4
Dmitriy Abragamov <dmitriy.abragamov@gusto.com>
2025-09-18 16:37:04 -0400

    feat: adding submission polling logic to payrollOverview

--- src/components/Payroll/PayrollOverview/PayrollOverview.tsx
+++ src/components/Payroll/PayrollOverview/PayrollOverview.tsx
@@ -3,9 +3,10 @@ import { usePayrollsGetSuspense } from '@gusto/embedded-api/react-query/payrolls
 import { useTranslation } from 'react-i18next'
 import { useBankAccountsGetSuspense } from '@gusto/embedded-api/react-query/bankAccountsGet'
 import { useEmployeesListSuspense } from '@gusto/embedded-api/react-query/employeesList'
+import { useState } from 'react'
 import { PayrollOverviewPresentation } from './PayrollOverviewPresentation'
-import { componentEvents } from '@/shared/constants'
-import { BaseComponent, type BaseComponentInterface } from '@/components/Base'
+import { componentEvents, PAYROLL_PROCESSING_STATUS } from '@/shared/constants'
+import { BaseComponent, useBase, type BaseComponentInterface } from '@/components/Base'
 import { useComponentDictionary, useI18n } from '@/i18n'
 
 interface PayrollOverviewProps extends BaseComponentInterface<'Payroll.PayrollOverview'> {
@@ -24,14 +25,36 @@ export function PayrollOverview(props: PayrollOverviewProps) {
 export const Root = ({ companyId, payrollId, dictionary, onEvent }: PayrollOverviewProps) => {
   useComponentDictionary('Payroll.PayrollOverview', dictionary)
   useI18n('Payroll.PayrollOverview')
+  const { baseSubmitHandler } = useBase()
   const { t } = useTranslation('Payroll.PayrollOverview')
+  const [isPolling, setIsPolling] = useState(false)
 
-  const { data } = usePayrollsGetSuspense({
-    companyId,
-    payrollId: payrollId,
-    include: ['taxes', 'benefits', 'deductions'],
-  })
+  const { data } = usePayrollsGetSuspense(
+    {
+      companyId,
+      payrollId: payrollId,
+      include: ['taxes', 'benefits', 'deductions'],
+    },
+    { refetchInterval: isPolling ? 5_000 : false },
+  )
   const payrollData = data.payrollShow!
+
+  //If payroll is in submitting state and we are not polling yet, start polling
+  if (
+    payrollData.processingRequest?.status === PAYROLL_PROCESSING_STATUS.submitting &&
+    !isPolling
+  ) {
+    setIsPolling(true)
+  }
+  //If we are polling and payroll is in success state, stop polling, and emit event
+  //Payroll is processed
+  if (
+    isPolling &&
+    payrollData.processingRequest?.status === PAYROLL_PROCESSING_STATUS.submit_success
+  ) {
+    onEvent(componentEvents.RUN_PAYROLL_PROCESSED)
+    setIsPolling(false)
+  }
 
   const { data: bankAccountData } = useBankAccountsGetSuspense({
     companyId,
@@ -42,7 +65,7 @@ export const Root = ({ companyId, payrollId, dictionary, onEvent }: PayrollOverv
     companyId,
   })
 
-  const { mutateAsync, isPending } = usePayrollsSubmitMutation()
+  const { mutateAsync: submitPayroll, isPending } = usePayrollsSubmitMutation()
 
   if (!payrollData.calculatedAt) {
     throw new Error(t('alerts.payrollNotCalculated'))
@@ -66,23 +89,27 @@ export const Root = ({ companyId, payrollId, dictionary, onEvent }: PayrollOverv
   const onEdit = () => {
     onEvent(componentEvents.RUN_PAYROLL_EDITED)
   }
+
   const onSubmit = async () => {
-    const result = await mutateAsync({
-      request: {
-        companyId,
-        payrollId,
-        requestBody: {
-          submissionBlockers: [],
+    await baseSubmitHandler(data, async () => {
+      const result = await submitPayroll({
+        request: {
+          companyId,
+          payrollId,
+          requestBody: {
+            submissionBlockers: [],
+          },
         },
-      },
+      })
+      onEvent(componentEvents.RUN_PAYROLL_SUBMITTED, result)
+      setIsPolling(true)
     })
-    onEvent(componentEvents.RUN_PAYROLL_SUBMITTED, result)
   }
   return (
     <PayrollOverviewPresentation
       onEdit={onEdit}
       onSubmit={onSubmit}
-      isSubmitting={isPending}
+      isSubmitting={isPending || isPolling}
       payrollData={payrollData}
       bankAccount={bankAccount}
       employeeDetails={employeeData.showEmployees || []}
