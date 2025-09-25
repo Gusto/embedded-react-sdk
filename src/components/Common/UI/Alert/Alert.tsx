31163bd7f22107657ccd1a4672a080c13462f8be
Jeff Johnson <jeff.johnson@gusto.com>
2025-09-25 09:59:47 -0700

    fix: vertically center alert header row

--- src/components/Common/UI/Alert/Alert.tsx
+++ src/components/Common/UI/Alert/Alert.tsx
@@ -40,15 +40,17 @@ export function Alert(rawProps: AlertProps) {
         ref={alertRef}
       >
         <div className={styles.header}>
-          <div className={styles.icon}>{icon || defaultIcon}</div>
-          <h6 id={id}>{label}</h6>
-          {onDismiss && (
-            <div className={styles.dismiss}>
-              <ButtonIcon variant="tertiary" onClick={onDismiss} aria-label="Dismiss alert">
-                <CloseIcon width={36} height={36} />
-              </ButtonIcon>
-            </div>
-          )}
+          <div className={styles.iconLabelContainer}>
+            <div className={styles.icon}>{icon || defaultIcon}</div>
+            <h6 id={id}>{label}</h6>
+            {onDismiss && (
+              <div className={styles.dismiss}>
+                <ButtonIcon variant="tertiary" onClick={onDismiss} aria-label="Dismiss alert">
+                  <CloseIcon width={36} height={36} />
+                </ButtonIcon>
+              </div>
+            )}
+          </div>
         </div>
         <div className={styles.content}>{children}</div>
       </div>
