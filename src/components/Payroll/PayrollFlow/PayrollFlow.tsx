6d6f64f90a8709d26ecd3a6a28ca77994de3c689
Steve Jensen <steve.jensen@gusto.com>
2025-10-17 13:22:10 -0600

    chore: add missing run payroll documentation

--- src/components/Payroll/PayrollFlow/PayrollFlow.tsx
+++ src/components/Payroll/PayrollFlow/PayrollFlow.tsx
@@ -5,19 +5,18 @@ import type { PayrollFlowProps } from './PayrollFlowComponents'
 import { PayrollLandingContextual, type PayrollFlowContextInterface } from './PayrollFlowComponents'
 import { Flow } from '@/components/Flow/Flow'
 
-export const PayrollFlow = ({ companyId, onEvent, defaultValues }: PayrollFlowProps) => {
+export const PayrollFlow = ({ companyId, onEvent }: PayrollFlowProps) => {
   const payrollFlow = useMemo(
     () =>
       createMachine('landing', payrollMachine, (initialContext: PayrollFlowContextInterface) => ({
         ...initialContext,
         component: PayrollLandingContextual,
         companyId,
-        defaultValues,
         totalSteps: 3,
         currentStep: 1,
         showProgress: false, // Landing step does not show progress bar
       })),
-    [companyId, defaultValues],
+    [companyId],
   )
   return <Flow machine={payrollFlow} onEvent={onEvent} />
 }
