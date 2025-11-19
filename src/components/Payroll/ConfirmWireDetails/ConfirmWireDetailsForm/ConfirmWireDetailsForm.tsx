06a97afff3814b27ab561f5d25a5ec8e7090a177
Dmitriy Abragamov <dmitriy.abragamov@gusto.com>
2025-11-19 11:25:45 -0500

    chore: stash pre-textarea

--- src/components/Payroll/ConfirmWireDetails/ConfirmWireDetailsForm/ConfirmWireDetailsForm.tsx
+++ src/components/Payroll/ConfirmWireDetails/ConfirmWireDetailsForm/ConfirmWireDetailsForm.tsx
@@ -2,16 +2,18 @@ import { useTranslation } from 'react-i18next'
 import z from 'zod'
 import { Form, FormProvider, useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useWireInRequestsSubmitMutation } from '@gusto/embedded-api/react-query/wireInRequestsSubmit'
+import styles from './ConfirmWireDetailsForm.module.scss'
 import { BaseComponent, useBase, type BaseComponentInterface } from '@/components/Base'
 import { useComponentContext } from '@/contexts/ComponentAdapter/useComponentContext'
 import { useComponentDictionary, useI18n } from '@/i18n'
 import { Flex, FlexItem } from '@/components/Common/Flex/Flex'
 import { payrollWireEvents } from '@/shared/constants'
+import { TextInputField } from '@/components/Common'
 
 interface ConfirmWireDetailsFormProps
   extends BaseComponentInterface<'Payroll.ConfirmWireDetailsForm'> {
-  companyId: string
-  wireInId?: string
+  wireInId: string
 }
 
 export const ConfirmWireDetailsFormSchema = z.object({
@@ -31,57 +33,73 @@ export function ConfirmWireDetailsForm(props: ConfirmWireDetailsFormProps) {
   )
 }
 
-const Root = ({ companyId, wireInId, dictionary }: ConfirmWireDetailsFormProps) => {
+const Root = ({ wireInId, dictionary }: ConfirmWireDetailsFormProps) => {
   useComponentDictionary('Payroll.ConfirmWireDetailsForm', dictionary)
   useI18n('Payroll.ConfirmWireDetailsForm')
   const { onEvent, baseSubmitHandler } = useBase()
   const { t } = useTranslation('Payroll.ConfirmWireDetailsForm')
-  const { Button, Heading, Text, F } = useComponentContext()
+  const { Button, Heading, Text } = useComponentContext()
 
   const formHandlers = useForm<ConfirmWireDetailsFormValues>({
     resolver: zodResolver(ConfirmWireDetailsFormSchema),
   })
 
+  const { mutateAsync: submitWireInRequest, isPending: isPendingSubmit } =
+    useWireInRequestsSubmitMutation()
+
   const onSubmit = async (data: ConfirmWireDetailsFormValues) => {
     await baseSubmitHandler(data, async innerData => {
+      await submitWireInRequest({
+        request: {
+          wireInRequestUuid: wireInId,
+          requestBody: innerData,
+        },
+      })
       onEvent(payrollWireEvents.PAYROLL_WIRE_FORM_DONE, innerData)
     })
   }
-
-  // const handleSubmit = () => {
-  //   onEvent(payrollWireEvents.PAYROLL_WIRE_FORM_DONE)
-  // }
 
   const handleCancel = () => {
     onEvent(payrollWireEvents.PAYROLL_WIRE_FORM_CANCEL)
   }
 
   return (
-    <Flex flexDirection="column" gap={24}>
-      <FlexItem>
-        <Heading as="h2">{t('title')}</Heading>
-        <Text>{t('description')}</Text>
-      </FlexItem>
+    <div className={styles.container}>
+      <Flex flexDirection="column" gap={24}>
+        <FlexItem>
+          <Heading as="h2">{t('title')}</Heading>
+          <Text>{t('description')}</Text>
+        </FlexItem>
 
-      {/* TODO: Form fields will be implemented in subsequent ticket */}
-      <FormProvider {...formHandlers}>
-        <Form>
-          <Flex flexDirection="column" gap={20}>
-            <TextInputField name="amountSent" label={t('amountLabel')} />
-            <TextInputField name="dateSent" label={t('dateLabel')} />
-            <TextInputField name="bankName" label={t('bankNameLabel')} />
-            <TextAreaField name="additionalNotes" label={t('notesLabel')} />
-          </Flex>
-        </Form>
-      </FormProvider>
-      <Flex gap={12} justifyContent="flex-end">
-        <Button variant="secondary" onClick={handleCancel}>
-          {t('cancelCta')}
-        </Button>
-        <Button variant="primary" onClick={handleSubmit}>
-          {t('submitCta')}
-        </Button>
+        <FormProvider {...formHandlers}>
+          <Form className={styles.form}>
+            <Flex flexDirection="column" gap={20}>
+              <TextInputField name="amountSent" label={t('amountLabel')} isRequired />
+              <TextInputField name="dateSent" label={t('dateLabel')} isRequired />
+              <TextInputField
+                name="bankName"
+                label={t('bankNameLabel')}
+                isRequired
+                description={t('bankNameDescription')}
+                placeholder={t('bankNamePlaceholder')}
+              />
+              {/* <TextAreaField name="additionalNotes" label={t('notesLabel')} /> */}
+            </Flex>
+          </Form>
+        </FormProvider>
+        <Flex gap={12} justifyContent="flex-end">
+          <Button variant="secondary" onClick={handleCancel} isLoading={isPendingSubmit}>
+            {t('cancelCta')}
+          </Button>
+          <Button
+            variant="primary"
+            onClick={formHandlers.handleSubmit(onSubmit)}
+            isLoading={isPendingSubmit}
+          >
+            {t('submitCta')}
+          </Button>
+        </Flex>
       </Flex>
-    </Flex>
+    </div>
   )
 }
