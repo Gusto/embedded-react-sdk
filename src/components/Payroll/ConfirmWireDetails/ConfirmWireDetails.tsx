8e58709f1063fb67794ed4888d396abc4cc14b95
Dmitriy Abragamov <dmitriy.abragamov@gusto.com>
2025-12-17 15:26:09 -0500

    fix: format

--- src/components/Payroll/ConfirmWireDetails/ConfirmWireDetails.tsx
+++ src/components/Payroll/ConfirmWireDetails/ConfirmWireDetails.tsx
@@ -64,7 +64,6 @@ function Root({ companyId, wireInId, onEvent }: ConfirmWireDetailsProps) {
   const [current, send] = useMachine(confirmWireDetailsMachineInstance)
 
   function handleEvent(type: EventType, data?: unknown) {
-     
     send({ type, payload: data })
 
     if (type === payrollWireEvents.PAYROLL_WIRE_START_TRANSFER) {
