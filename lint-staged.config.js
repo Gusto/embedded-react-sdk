16ec89bb240d3f8c14f9a391bc457816a422df40
Steve Jensen <steve.jensen@gusto.com>
2025-12-17 16:23:05 -0700

    chore: fix tree shaking and add pagination to adapter docs

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,16 @@
 export default {
   '*.ts*': ['npm run build . --', 'npm run format --', 'npm run lint --'],
+  '*.md': ['npm run format --'],
   'src/components/Common/UI/**/*Types.ts': () => [
     'npm run adapter:docs:generate',
-    'prettier docs/component-adapter/component-inventory.md --write --log-level error',
+    'git add docs/component-adapter/component-inventory.md',
+  ],
+  'src/contexts/ComponentAdapter/componentAdapterTypes.ts': () => [
+    'npm run adapter:docs:generate',
+    'git add docs/component-adapter/component-inventory.md',
+  ],
+  'build/generateAdapterPropDocs.ts': () => [
+    'npm run adapter:docs:generate',
     'git add docs/component-adapter/component-inventory.md',
   ],
 }
