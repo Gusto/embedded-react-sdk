085c78728fd33a3c6eb74805caea811d7e693818
Steve Jensen <steve.jensen@gusto.com>
2026-01-05 18:30:13 -0700

    feat: add skeleton for information requests

---
+++ src/components/Payroll/InformationRequests/InformationRequestsComponents.tsx
@@ -0,0 +1,27 @@
+import { InformationRequestForm } from './InformationRequestForm'
+import { useFlow } from '@/components/Flow/useFlow'
+import { ensureRequired } from '@/helpers/ensureRequired'
+import type { FlowContextInterface } from '@/components/Flow/useFlow'
+import type { CommonComponentInterface } from '@/components/Base'
+import type { EventType } from '@/types/Helpers'
+import type { OnEventType } from '@/components/Base/useBase'
+
+export interface InformationRequestsContextInterface extends FlowContextInterface {
+  companyId: string
+  selectedRequestId?: string
+  component:
+    | (React.ComponentType<CommonComponentInterface> & {
+        Footer?: React.ComponentType<{
+          onEvent: OnEventType<EventType, unknown>
+        }>
+      })
+    | null
+}
+
+export function InformationRequestFormContextual() {
+  const { selectedRequestId, onEvent } = useFlow<InformationRequestsContextInterface>()
+
+  return <InformationRequestForm requestId={ensureRequired(selectedRequestId)} onEvent={onEvent} />
+}
+
+InformationRequestFormContextual.Footer = InformationRequestForm.Footer
