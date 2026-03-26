50503a076ca0407c688b58c653999c63b2d763ac
Kristine White <kristine.white@gusto.com>
2026-03-26 16:40:35 -0700

    feat: state machine and skeleton ui

---
+++ src/components/TimeOff/AddEmployeesToPolicy/AddEmployeesToPolicy.tsx
@@ -0,0 +1,29 @@
+import { BaseComponent, type BaseComponentInterface } from '@/components/Base'
+import { componentEvents } from '@/shared/constants'
+
+export interface AddEmployeesToPolicyProps extends BaseComponentInterface {
+  policyId: string
+}
+
+export function AddEmployeesToPolicy(props: AddEmployeesToPolicyProps) {
+  return (
+    <BaseComponent {...props}>
+      <div>
+        <p>Add Employees to Policy + Starting Balances (policyId: {props.policyId})</p>
+        <button onClick={() => { props.onEvent(componentEvents.TIME_OFF_ADD_EMPLOYEES_DONE); }}>
+          Done
+        </button>
+        <button
+          onClick={() =>
+            { props.onEvent(componentEvents.TIME_OFF_ADD_EMPLOYEES_ERROR, {
+              alert: { type: 'error', title: 'Failed to add employees' },
+            }); }
+          }
+        >
+          Simulate Error
+        </button>
+        <button onClick={() => { props.onEvent(componentEvents.CANCEL); }}>Cancel</button>
+      </div>
+    </BaseComponent>
+  )
+}
